4dafe46bb9081c8df7c24e7cd81ff3ee574dbda2
Bianzon-DCET <kcbianzon@gmail.com>
2025-06-13 20:51:45 +0800

    added instructions

--- .history/project_20250613204607.js
+++ .history/project_20250613204607.js
@@ -47,5 +47,21 @@ function login() {
       return;
     }
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    var acc = document.getElementsByClassName("accordion");
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].onclick = function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+          panel.style.maxHeight = null;
+        } else {
+          panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+      };
+    }
+  });
+
   alert("Invalid credentials!");
 }
